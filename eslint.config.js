092f6741b2eef01c2770fac375df2fb0155c26d2
Annapirz <annapirz@Air-Annapirz.Dlink>
2024-11-03 17:21:45 +0300

    start prod

---
+++ eslint.config.js
@@ -0,0 +1,36 @@
+import js from "@eslint/js";
+
+export default [
+    
+    // apply recommended rules to JS files
+    {
+        files: ["**/*.js"],
+        rules: js.configs.recommended.rules
+    },
+    
+    // apply recommended rules to JS files with an override
+    {
+        files: ["**/*.js"],
+        rules: {
+            ...js.configs.recommended.rules,
+            "no-unused-vars": "warn"
+        }
+    },
+    
+    // apply all rules to JS files
+    {
+        files: ["**/*.js"],
+        rules: {
+            ...js.configs.all.rules,
+            "no-unused-vars": "warn"
+        }
+    },
+    
+    // no var used
+    {
+        files: ["**/*.js"],
+        rules: {
+            'no-var': "error",
+        },
+    },
+]
