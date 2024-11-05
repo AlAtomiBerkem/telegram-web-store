bcbaba81434c7f554e90f20ba6821f71f07b8c2e
Annapirz <annapirz@MacBook-Air-Annapirz.local>
2024-11-05 14:22:15 +0300

    feat: statick, add hrader/button bot

--- index.js
+++ index.js
@@ -2,23 +2,31 @@ const token = '7630284623:AAGUOCzhvE1SP_Vhg3REyS7HPs205xVZ_e0'
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, {polling: true});
-const webAppUrl = '/';
+const webAppUrl = 'https://jocular-lily-d7ad19.netlify.app/'
+
+
+
  bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
     
-    // if (text === '/start') {
-    //     await bot.sendMessage(chatId, 'ниже появится конопка заполнить', {
-    //         reply_markup: {
-    //             keyboard: [
-    //                 [{text: 'заполнить форму'}],
-    //                 ],
-    //             inline_keyboard: [
-    //                 [{text: 'заполнить форму', web_app: {url: webAppUrl}}],
-    //             ]
-    //         }
-    //     });
-    // }
+    if (text === '/start') {
+        await bot.sendMessage(chatId, 'ниже появится конопка заполнить', {
+            reply_markup: {
+                keyboard: [
+                    [{text: 'сделать заказ', web_app: {url: webAppUrl}}],
+                ],
+            },
+        });
+        await bot.sendMessage(chatId, 'ниже появится конопка заполнить', {
+            reply_markup: {
+                inline_keyboard: [
+                    [{text: 'заполнить форму', web_app: {url: webAppUrl}}],
+                ],
+            },
+        });
+    }
+    
     
     bot.sendMessage(chatId, 'здарова заебал');
     console.log('ответ отправлен');
