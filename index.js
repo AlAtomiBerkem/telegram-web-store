dfd051eed1e619095b8447a6317b64874b2b4e15
annapirz <fanworld991@gmail.com>
2025-01-09 20:59:22 +0300

    feat: remove trash

--- index.js
+++ index.js
@@ -2,13 +2,12 @@ const token = '7630284623:AAGUOCzhvE1SP_Vhg3REyS7HPs205xVZ_e0'
 
 const TelegramBot = require('node-telegram-bot-api');
 const bot = new TelegramBot(token, {polling: true});
-const webAppUrl = 'https://storied-bombolone-37d940.netlify.app/'
+const webAppUrl = 'https://storied-bombolone-37d940.netlify.app'
 
-
-
+console.log('server started!')
  bot.on('message', async (msg) => {
-    const chatId = msg.chat.id;
-    const text = msg.text;
+     const chatId = msg.chat.id;
+     const text = msg.text;
     
     if (text === '/start') {
         await bot.sendMessage(chatId, 'ниже появится конопка заполнить', {
@@ -25,10 +24,25 @@ const webAppUrl = 'https://storied-bombolone-37d940.netlify.app/'
                 ],
             },
         });
+        console.log('click');
+        
+        if (msg?.web_app_data?.data) {
+            try{
+                const data = JSON.parse(msg?.web_app_data?.data);
+               await bot.sendMessage(chatId, 'Спасибо за обратную связь!');
+               await bot.sendMessage(chatId,'Ваша страна' + data.coutry);
+               cosnole.log(data.coutry);
+               await bot.sendMessage(chatId,'Ваша ваша улица' + data.street);
+               console.log(data.street);
+               co
+               
+                setTimeout(async () => {
+                    await bot.sendMessage(chatId,'всю информацию вы получите в этом чате');
+                }, 3000)
+            } catch (e) {
+                console.log('Error! - ', e)
+            }
+        }
     }
-    
-    
-    
-    bot.sendMessage(chatId, 'здарова заебал');
     console.log('ответ отправлен');
 });
